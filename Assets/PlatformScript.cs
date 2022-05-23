70b8bb30fcc2401f35fbdf703bd64ef6638fff6e
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-05-23 12:10:06 -0300

    Vector3 ejercicios
    finished

---
+++ Assets/PlatformScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public float xSpeed;
+    public float xRightLimit;
+    public float xLeftLimit;
+    public bool toRight;
+    public GameObject prefabPelota;
+    public GameObject prefabCilindro;
+    public GameObject baseDer;
+    public GameObject baseIzq;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        toRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toRight)
+        {
+            transform.position += new Vector3(xSpeed, 0, 0);            
+        }
+
+        else
+        {
+            transform.position -= new Vector3(xSpeed, 0, 0);
+        }
+        
+        if (transform.position.x >= xRightLimit)
+        {
+            toRight = false;
+            GameObject clon;
+            clon =Instantiate(prefabPelota);
+            clon.transform.position = baseDer.transform.position + new Vector3 (0, 1, 0);
+        }
+
+        else if (transform.position.x <= xLeftLimit)
+        {
+            toRight = true;
+            GameObject clon;
+            clon = Instantiate(prefabCilindro);
+            clon.transform.position = baseIzq.transform.position + new Vector3(0, 1, 0);
+        }
+    }
+}
